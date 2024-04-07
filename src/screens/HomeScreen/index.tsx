60c45eacc6a593d6f39b61ef9474cb651ce40f11
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2024-04-07 14:11:29 +0530

    fixed till home screen navigations

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -6,11 +6,16 @@ import { RootDrawerParamList, RootStackParamList } from '../../types';
 type HomeProps = NativeStackScreenProps<RootDrawerParamList, "HomeScreen">
 
 
+
 const HomeScreen = ({navigation}:HomeProps) => {
   
   return (
     <View style={styles.container} >
-      <Text>Welcome to Home Screen!</Text>
+      <View style={styles.textContainer} >
+
+      <Text style={styles.textStyle}>List of shop Categories + Importat Contacts</Text>
+      <Text style={styles.textStyle}>e.g. Shoes, Watch, Cloth, Mats + Contacts of Potters, Kabadi waale, R.O. , AC, Electrician</Text>
+      </View>
    
     </View>
   );
@@ -24,5 +29,16 @@ const styles = StyleSheet.create({
     alignItems:"center",
     justifyContent:"center"
   
+  },
+  textContainer:{
+    width:250,
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+    textAlign:"center"
+  },
+  textStyle:{
+    fontSize:20,
+    textAlign:"center"
   }
 })
