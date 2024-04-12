ed474efcffff2efb8fe8b172ab6fcbae4f07ac14
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2024-04-13 00:28:59 +0530

    made some changes

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,27 +1,28 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { RootDrawerParamList, RootStackParamList } from '../../types';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button, Alert, PermissionsAndroid } from 'react-native';
+import { RootDrawerParamList } from '../../types'; // Assuming you only need RootDrawerParamList
+import MapView from 'react-native-maps';
 
-type HomeProps = NativeStackScreenProps<RootDrawerParamList, "HomeScreen">
+type HomeProps = NativeStackScreenProps<RootDrawerParamList, "HomeScreen">;
+
+const HomeScreen = ({ navigation }: HomeProps) => {
 
 
 
-const HomeScreen = ({navigation}:HomeProps) => {
-  
+
+
+
   return (
     <View style={styles.container} >
       <View style={styles.textContainer} >
-
-      <Text style={styles.textStyle}>List of shop Categories + Importat Contacts</Text>
-      <Text style={styles.textStyle}>e.g. Shoes, Watch, Cloth, Mats + Contacts of Potters, Kabadi waale, R.O. , AC, Electrician</Text>
       
-      <Button
-      title='Go to ShopList Screen'
-      onPress={()=>navigation.navigate("ShopListScreen")}
-      />
+        <Button
+          title='Go to ShopList Screen'
+          onPress={() => navigation.navigate("ShopListScreen")}
+        />
+    
       </View>
-   
     </View>
   );
 };
@@ -29,21 +30,20 @@ const HomeScreen = ({navigation}:HomeProps) => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center"
-  
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
-  textContainer:{
-    width:250,
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
-    textAlign:"center"
+  textContainer: {
+    width: 250,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center"
   },
-  textStyle:{
-    fontSize:20,
-    textAlign:"center"
+  textStyle: {
+    fontSize: 20,
+    textAlign: "center"
   }
-})
+});
