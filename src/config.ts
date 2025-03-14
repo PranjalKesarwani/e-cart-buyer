175906f9c0b08a8fb5cc18d8a949c664c69faa03
Pranjal Kesarwani <pranjalkesarwani2000@gmail.com>
2025-03-14 12:21:33 +0530

    refactor: update API_URL in config and upgrade axios and react-native-community/cli dependencies

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@
 // const isDevelopment = APP_ENV === 'development' || __DEV__;
 
 // export const API_URL = isDevelopment ? 'http://localhost:8080/api' : ProdApiUrl;
+// export const API_URL = 'http://localhost:8080/api/v1';
 export const API_URL = 'http://10.0.2.2:8080/api/v1';
