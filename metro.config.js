c885f8e51f9e9ebb53f4dec1f56f255a3f9905b5
Pranjal Kesarwani <pranjalkesarwani2000@gmail.com>
2025-10-05 11:35:01 +0530

    working

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
 /**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
+ * Minimal metro.config.js for React Native 0.81.x
  */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const { getDefaultConfig } = require('@react-native/metro-config');
+module.exports = getDefaultConfig(__dirname);
