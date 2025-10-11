6d05a35a7e0c4e7fa5981dfdbe03ec1eee6a37f5
Pranjal Kesarwani <pranjalkesarwani2000@gmail.com>
2025-10-11 18:27:22 +0530

    fixed versions of package.json

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  project: {
-    android: {
-      packageName: "com.buyer",
+    project: {
+        ios: {}, // Keep this if you have iOS files, even if empty
+        android: {
+            // *** ADD YOUR PACKAGE NAME HERE ***
+            packageName: 'com.buyer', // <--- REPLACE 'com.ecartbuyer' with your actual package name!
+        },
     },
-  },
+    // If you have custom assets, they go here (e.g., vector-icons fonts)
+    assets: ['./assets'],
 };
